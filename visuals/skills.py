d522962a8d42fe1aaf21007eddfc3ad1cbc55a8e
Kabir Ansari <kabir.ansari@outlook.com>
2026-04-26 10:16:15 +0100

    Update skills.py

--- visuals/skills.py
+++ visuals/skills.py
@@ -84,7 +84,6 @@ def show():
             xaxis_tickangle=-30,
             xaxis_title="Skill Category",
             yaxis_title="Number of<br>postings",
-            yaxis=dict(rangemode="tozero"),
             title_x=0.5,
             title_font=dict(size=22),
             xaxis=dict(title_font=dict(size=16), tickfont=dict(size=13)),
