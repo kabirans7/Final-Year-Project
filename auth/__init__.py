86e97be83ae7d0f3bbd14456d0bf196ff6b6786c
Kabir Ansari <kabir.ansari@outlook.com>
2026-04-27 11:11:24 +0100

    Comment out admin codes for future integration
    Comment out future integration code for admin features.

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,2 +1,3 @@
-from .auth_state import init_auth_state, logout_button_top_right, require_auth
-from .auth_router import render_auth_router
+# Admin Codes - Will be integrated in future iteration 
+# from .auth_state import init_auth_state, logout_button_top_right, require_auth
+# from .auth_router import render_auth_router
